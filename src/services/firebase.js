5164b9a88b7ae60bdbe7a11a7f45e2618447ba23
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2020-07-16 13:40:08 +0100

    added chat page

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase'
-
+import firebase from 'firebase/app'
+import "firebase/auth";
+import "firebase/database"
 
 const config = {
     apiKey: "AIzaSyA60htlAtP6hDXjnlhppCeHzFO-BPwbF7o",
